151bc685ea5a4bfdef4546c7e78c9cc4432368e3
Mark Palmeri <mlp6@duke.edu>
2018-02-28 13:33:40 -0500

    Create Main.py

---
+++ HRM_Main.py
@@ -0,0 +1,12 @@
+#the main code that will import the files
+def main():
+
+def readCSV():
+    pass
+
+def createHRM_Class():
+    pass
+
+#not sure what the code below is for
+if __name__ == '__main__':
+    main()
