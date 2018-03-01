17f11590d874e5f68694df5eda403e3fc5c30a04
Mark Palmeri <mlp6@duke.edu>
2018-02-28 20:15:35 -0500

    Updated Main Code Outline

--- HRM_Main.py
+++ HRM_Main.py
@@ -1,8 +1,23 @@
 #the main code that will import the files
-def main():
+import pandas as pd
 
-def readCSV():
-    pass
+def main():
+    filenames = collect_csv_file()
+    for files in filenames:
+        temp = pd.read_csv(files, delimiter=',')
+
+def collect_csv_file():
+    """Function that collects all the .csv filenames in the data folder.
+
+    :returns: list(str) of all .csv filenames
+    """
+    #for future projects, if not csv, function that finds the extension can be made
+    import glob
+    filenames = glob.glob('data/*csv')
+    return filenames
+
+def processCSV():
+    from processCSV import processCSV
 
 def createHRM_Class():
     pass
