427c04f79e1023791661200509a181d8e592a07b
Mark Palmeri <mlp6@duke.edu>
2018-02-28 21:12:56 -0500

    Main function can create HrmClass now

--- HRM_Main.py
+++ HRM_Main.py
@@ -5,13 +5,18 @@ def main():
     filenames = collect_csv()
     for files in filenames:
         temp = pd.read_csv(files, delimiter=',', names=['time', 'volt'])
+        hrm_info = createHRM_Class(files, temp)
+        #createJSON()
 
 def collect_csv():
     from collect_csv_file import collect_csv_file
     return collect_csv_file()
 
-def createHRM_Class():
-    pass
+def createHRM_Class(name, data):
+    import os
+    from hrmclass import HrmClass
+    path, ext = os.path.splitext(name)
+    return HrmClass(path, data.time, data.volt)
 
 def createJSON():
     pass
