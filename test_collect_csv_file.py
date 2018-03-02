c996a8fca3dae9d62cf115cb7408642826334bbc
Mark Palmeri <mlp6@duke.edu>
2018-03-02 14:12:30 -0500

    Added docstrings, logs, try/excepts, and test

---
+++ test_collect_csv_file.py
@@ -0,0 +1,20 @@
+def test_exceptions():
+    import pytest
+    import math
+    with pytest.raises(ImportError, message="Expecting ImportError"):
+        import blah
+    with pytest.raises(TypeError, message="Expecting TypeError"):
+        test = 5 + 'h'
+    with pytest.raises(ValueError, message="Expecting ValueError"):
+        test = math.sqrt(-1)
+
+def test_filenames_not_empty():
+    from collect_csv_file import collect_csv_file
+    names = collect_csv_file()
+    assert names != None
+
+def test_list_of_strings():
+    from collect_csv_file import collect_csv_file
+    names = collect_csv_file()
+    tester = ['string', 'string3']
+    assert type(names) == type(tester)
