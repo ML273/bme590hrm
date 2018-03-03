90502ac33e1aa1cb52a424de7b5835c810148da3
Marianne Lee <ml273@duke.edu>
2018-03-03 03:11:15 -0500

    Passed tests, manipulated pandas DataFrames, PEP8 checked

---
+++ cleaner.py
@@ -0,0 +1,69 @@
+def cleaner(df):
+    """Function that cleans up time and voltage data
+
+    :param df: Pandas DataFrame with Time and Voltage Columns
+
+    :returns: numpy.array of a clean time vector
+    :returns: numpy.array of a clean voltage vector
+    :raises ImportError: error raised if missing package
+    :raises TypeError: error raised if input is not pandas dataframe
+    """
+    import logging
+    logging.basicConfig(
+        filename="datacleaner.log", format='%(levelname)s \
+        %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %pi')
+    logging.info('Begin Cleaning')
+    logging.info('Attempt importing')
+    try:
+        import numpy as np
+        import pandas as pd
+        import math
+    except ImportError:
+        print("Check that you have numpy, math, pandas to import")
+        logging.debug('Add numpy, math, pandas packages')
+    try:
+        logging.info('Check if df is a pandas dataframe')
+        isinstance(df, pd.DataFrame)
+    except TypeError:
+        print("Variable df is not a pandas dataframe")
+        logging.error('df is not a pandas dataframe')
+    for i, t in enumerate(df.time):
+        if not isfloat(df.time[i]):
+            logging.warning('Your data may be corrupted.')
+            logging.info(
+                'Changing index ' + str(i) + ' from ' + str(t) + 'to 0.')
+            df.time[i] = 0
+        elif math.isnan(float(df.time[i])):
+            logging.warning('Your data may be corrupted.')
+            logging.info(
+                'Changing index ' + str(i) + ' from ' + str(t) + 'to 0.')
+            df.time[i] = 0
+    for j, v in enumerate(df.volt):
+        if not isfloat(df.volt[j]):
+            logging.warning('Your data may be corrupted.')
+            logging.info(
+                'Changing index ' + str(j) + ' from ' + str(v) + 'to 0.')
+            df.volt[j] = 0
+        elif math.isnan(float(df.volt[j])):
+            logging.warning('Your data may be corrupted.')
+            logging.info(
+                'Changing index ' + str(j) + ' from ' + str(v) + 'to 0.')
+            df.volt[j] = 0
+    logging.info('End of converting bad data and NaN')
+    logging.info('Create numpy arrays')
+    nptime = np.array(df.time)
+    npvolt = np.array(df.volt)
+    logging.info('Change numpy arrays to float type')
+    return (nptime.astype(float), npvolt.astype(float))
+
+
+def isfloat(value):
+    """Simple function that checks for floats
+
+    :param value: single str, int, or float allowed
+    """
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
---
+++ test_cleaner.py
@@ -0,0 +1,37 @@
+def test_exceptions():
+    import pytest
+    import math
+    with pytest.raises(ImportError, message="Expecting ImportError"):
+        import blah
+    with pytest.raises(TypeError, message="Expecting TypeError"):
+        test = 5 + 'h'
+    with pytest.raises(ValueError, message="Expecting ValueError"):
+        test = math.sqrt(-1)
+
+
+def test_float():
+    from cleaner import isfloat
+    assert(not isfloat('string'))
+
+
+def test_float2():
+    from cleaner import isfloat
+    assert(isfloat('456'))
+
+
+def test_float3():
+    from cleaner import isfloat
+    import numpy as np
+    assert(isfloat(np.nan))
+
+
+def test_clean():
+    import glob
+    import numpy as np
+    import pandas as pd
+    from cleaner import cleaner, isfloat
+    file1 = 'data/test_data30.csv'
+    df = pd.read_csv(file1, names=['time', 'volt'])
+    nptime, npvolt = cleaner(df)
+    assert abs(sum(nptime) - 199976.1) < 0.2 \
+        and abs(sum(npvolt) - 443.105) < 0.01
