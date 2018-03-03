2911e31f8f989712f3e2c42aa3e8149aa1e93fc0
Marianne Lee <ml273@duke.edu>
2018-03-02 20:01:29 -0500

    Recreated Main because I accidentally deleted it earlier

---
+++ HRM_Main.py
@@ -0,0 +1,26 @@
+# main code that will import the files
+import pandas as pd
+import os
+import numpy as np
+
+def main():
+    filenames = collect_csv()
+    for files in filenames:
+        temp = pd.read_csv(files, delimiter=',', names=['time', 'volt'])
+        hrm_info = createHRM_Class(files, temp)
+        #createJSON()
+        
+def collect_csv():
+    from collect_csv_file import collect_csv_file
+    return collect_csv_file()
+    
+def createHRM_Class(name, data):
+    from hrmclass import HrmClass
+    path, ext = os.path.splitext(name)
+    return HrmClass(path, np.array(data.time), np.array(data.volt))
+    
+def createJSON():
+    pass
+    
+if __name__ == '__main__':
+    main()
